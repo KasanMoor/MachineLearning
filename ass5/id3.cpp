270d41831667b3db373c34dab6440e85b5385b59
kasanmoor <brandonb.junk@gmail.com>
2017-04-12 22:46:57 -0700

    save

--- ass5/id3.cpp
+++ ass5/id3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "mat.h"
 #include "rand.h"
@@ -6,9 +7,53 @@
 
 using namespace std;
 
+vector<string> features;
+vector< vector<string> > featureValues;
+vector< vector<string> > data;
+
+void printVector( vector< vector<string> > features )
+{
+   for(int i=0; i<features.size(); i++){
+      for(int j=0; j<features[i].size(); j++){
+         cout << features[i][j] << " ";
+      }
+      cout << endl;
+   }
+}
+
+void readProblem()
+{
+   int numFeatures, numFeatureValues, numExamples;
+   string input;
+   cin >> numFeatures;
+   for(int i=0; i<numFeatures+1; i++) {
+      cin >> input;
+      features.push_back(input);
+      featureValues.push_back(vector<int>());
+      cin << numFeatureValues;
+      for(int j=0; j<numFeatureValues; j++) {
+         cin >> input;
+	 featureValues[i].push_back(input);
+      }
+   }
+   printVector(featureValues);
+}
+
+double gain()
+{
+}
+
+Tree *ID3BuildTree()
+{
+}
+
 int main()
 {
-   Tree* decisionTree = new Tree("decision Tree");
+   readProblem();
+   //Tree *DTree = ID3BuildTree();
+   //DTree->printAux();
+
+   /*Tree* decisionTree = new Tree("decision Tree");
    decisionTree->addChild("child", new Tree("here i am"));
    decisionTree->addChild("child", new Tree("here i am"));
    decisionTree->addChild("child", new Tree("here i am"));
@@ -20,4 +65,5 @@ int main()
 			       "second edge",
 			       new Tree("child 2"));
    secondTree->printEdge();
+   */
 }
